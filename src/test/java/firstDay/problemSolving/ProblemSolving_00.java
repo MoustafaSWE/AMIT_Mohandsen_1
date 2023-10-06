101bcb49c63a1fac071f4351165639b4bdd01cc2
moustafa-ismail <esmailm@extenad.com>
2023-10-06 18:10:33 +0300

    update project

---
+++ src/test/java/firstDay/problemSolving/ProblemSolving_00.java
@@ -0,0 +1,16 @@
+package firstDay.problemSolving;
+
+public class ProblemSolving_00 {
+
+    /*
+    Write a Java program to print 'Hello' on screen and your name on a separate line.
+
+        Expected Output :
+        Hello
+        Alexandra Abramov
+     */
+
+    public static void main(String[] args) {
+
+    }
+}
--- src/test/java/firstDay/problemSolving/ProblemSolving_01.java
+++ src/test/java/firstDay/problemSolving/ProblemSolving_01.java
@@ -1,13 +1,14 @@
 package firstDay.problemSolving;
 
-public class ProblemSolving {
+public class ProblemSolving_01 {
 
     /*
     Write a Java program to accept a number and check whether the number is even or not. Prints 1 if the number is even or 0 if odd.
 
     Sample Output:
     Input a number: 20
-    1
+
+    --> 1
      */
     public static void main(String[] args) {
 
